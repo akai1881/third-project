5fdd98abf660ccbc1c3b4660b0ccccbc7ba5c6d2
Kai <nomadwarboy@gmail.com>
2020-09-06 01:48:37 +0600

    added carousel and started catalog section

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
-console.log('hello world');
-// alert('hi');
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        infinite: true,
+        speed: 800,
+        slidesToShow: 1,
+        adaptiveHeight: false,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left_arrow.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right_arrow.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                  dots: true,
+                  arrows: false
+                }
+            }
+        ]
+    });
+  });
