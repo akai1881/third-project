df3713f0b47df3221590ffa5278fa3e4eb98ec40
Kai <nomadwarboy@gmail.com>
2020-09-10 15:28:37 +0600

    new tabs and catalog 1.0

--- js/script.js
+++ js/script.js
@@ -1,19 +1,37 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        infinite: true,
-        speed: 800,
-        slidesToShow: 1,
-        adaptiveHeight: false,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left_arrow.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right_arrow.svg"></button>',
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                  dots: true,
-                  arrows: false
-                }
-            }
-        ]
-    });
+// $(document).ready(function(){
+//     $('.carousel__inner').slick({
+//         infinite: true,
+//         speed: 800,
+//         slidesToShow: 1,
+//         adaptiveHeight: false,
+//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left_arrow.svg"></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="icons/right_arrow.svg"></button>',
+//         responsive: [
+//             {
+//                 breakpoint: 991,
+//                 settings: {
+//                   dots: true,
+//                   arrows: false
+//                 }
+//             }
+//         ]
+//     });
+//   });
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false,
+    speed: 500
   });
+
+  document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+  });
+
+  document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+  });
+
