df5495ad902ca4d3778633253e187cc903403551
Kai <nomadwarboy@gmail.com>
2020-09-15 00:39:40 +0600

    made slider on slick

--- js/script.js
+++ js/script.js
@@ -1,37 +1,54 @@
-// $(document).ready(function(){
-//     $('.carousel__inner').slick({
-//         infinite: true,
-//         speed: 800,
-//         slidesToShow: 1,
-//         adaptiveHeight: false,
-//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left_arrow.svg"></button>',
-//         nextArrow: '<button type="button" class="slick-next"><img src="icons/right_arrow.svg"></button>',
-//         responsive: [
-//             {
-//                 breakpoint: 991,
-//                 settings: {
-//                   dots: true,
-//                   arrows: false
-//                 }
-//             }
-//         ]
-//     });
-//   });
-const slider = tns({
-    container: '.carousel__inner',
-    items: 1,
-    slideBy: 'page',
-    autoplay: false,
-    controls: false,
-    nav: false,
-    speed: 500
-  });
+$(document).ready(function () {
+	$(".slider__inner").slick({
+		infinite: false,
+		speed: 300,
+		prevArrow:
+			'<button type="button" class="slick-prev"><img src="icons/left_arrow.svg" alt="left arrow"></button>',
+		nextArrow:
+			'<button type="button" class="slick-next"><img src="icons/right_arrow.svg" alt="left arrow"></button>',
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					dots: true,
+					arrows: false,
+				},
+			},
+		],
+	});
+});
 
-  document.querySelector('.prev').addEventListener('click', function () {
-    slider.goTo('prev');
-  });
+$(document).ready(function () {
+	$("ul.catalog__tabs").on(
+		"click",
+		"li:not(.catalog__tab_active)",
+		function () {
+			$(this)
+				.addClass("catalog__tab_active")
+				.siblings()
+				.removeClass("catalog__tab_active")
+				.closest("div.container")
+				.find("div.catalog__content")
+				.removeClass("catalog__content_active")
+				.eq($(this).index())
+				.addClass("catalog__content_active");
+		}
+	);
+	$(document).on("click", "div.carousel__nav .dot", function () {
+		$(this).addClass("dot_active").siblings().removeClass("dot_active");
+	});
+	function toggleSlide(item) {
+		$(item).each(function (i) {
+			$(this).on("click", function (e) {
+				e.preventDefault();
+				$(".catalog-item__content")
+					.eq(i)
+					.toggleClass("catalog-item__content_active");
+				$(".catalog-item__list").eq(i).toggleClass("catalog-item__list_active");
+			});
+		});
+	}
 
-  document.querySelector('.next').addEventListener('click', function () {
-    slider.goTo('next');
-  });
-
+	toggleSlide(".catalog-item__link");
+	toggleSlide(".catalog-item__link_back");
+});
